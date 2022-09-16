f34cb6fe8a6720ea5c75aaecde7ecae3e3405f46
james.patmore <110008934+jamestpatmore@users.noreply.github.com>
2022-09-15 20:47:52 -0700

    jamespush

---
+++ homepage.js
@@ -0,0 +1,9 @@
+function burgerToggle() {
+  var burger = document.querySelector('.navbar-burger');
+  var menu = document.querySelector('.navbar-menu');
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+}
+burgerToggle();
