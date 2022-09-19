f82d5e668b167147cb74be63d682eb64c835a94b
james.patmore <110008934+jamestpatmore@users.noreply.github.com>
2022-09-19 15:36:12 -0700

    final

--- signup.js
+++ signup.js
@@ -1,51 +1,38 @@
-function store(){
+// reference onclick for register for the function 
+// use the user input for the value const/var var.value 
+// local the storage the input 
+// make an object that bundles the user input and JSON.stringify() 
+// save it to local 
+// document the fname from the imput below register "... + fname + ...".
+// make speedrunner button 
+// when they click the button it clears the local storage input 
+// then redirects 
+const inpEmail = document.getElementById('email');
+const inpFirstName = document.getElementById('fname');
+const inpLastName = document.getElementById('lname');
+    
+const registerButton = document.getElementById('rgstr_btn');
+const output = document.getElementById('emailSuccess');
+   
+registerButton.onclick = function() {
+    const email = inpEmail.value;
+    const name = inpFirstName.value;
 
-    var name = document.getElementById('name');
-    var pw = document.getElementById('pw');
-    var lowerCaseLetters = /[a-z]/g;
-    var upperCaseLetters = /[A-Z]/g;
-    var numbers = /[0-9]/g;
+    console.log(email);
+    console.log(name);
 
-    if(name.value.length == 0){
-        alert('Please fill in email');
-
-    }else if(pw.value.length == 0){
-        alert('Please fill in password');
-
-    }else if(name.value.length == 0 && pw.value.length == 0){
-        alert('Please fill in email and password');
-
-    }else if(pw.value.length > 8){
-        alert('Max of 8');
-
-    }else if(!pw.value.match(numbers)){
-        alert('please add 1 number');
-
-    }else if(!pw.value.match(upperCaseLetters)){
-        alert('please add 1 uppercase letter');
-
-    }else if(!pw.value.match(lowerCaseLetters)){
-        alert('please add 1 lovercase letter');
-
-    }else{
-        localStorage.setItem('name', name.value);
-        localStorage.setItem('pw', pw.value);
-        alert('Your account has been created');
+    if(name && email) {
+        localStorage.setItem(email, name);
+        output.innerHTML += `Thank you for signing up to our newsletter ${name} !!`
     }
 }
 
-//checking
-function check(){
-    var storedName = localStorage.getItem('name');
-    var storedPw = localStorage.getItem('pw');
+const goHome = document.getElementById('homeButton')
 
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPw');
-    var userRemember = document.getElementById("rememberMe");
-
-    if(userName.value == storedName && userPw.value == storedPw){
-        window.location.assign('index.html');
-    }else{
-        alert('Error on login');
-    }
+goHome.onclick = function(event) {
+    event.preventDefault();
+    localStorage.clear();
+    window.location.assign('index.html');
 }
+
+
